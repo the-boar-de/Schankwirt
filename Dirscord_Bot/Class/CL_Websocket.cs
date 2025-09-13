b15137666cbe73562caebd522405ef5fe6b03276
TheBoarDE _ <chris.kett07@gmail.com>
2025-09-13 06:57:59 +0200

    -enhancement-
    added visual studio project

---
+++ Dirscord_Bot/Class/CL_Websocket.cs
@@ -0,0 +1,46 @@
+//standard system refernces
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//own refernces
+
+//project refernces
+using Discord;
+using Discord.WebSocket;
+
+//Class namespase (refernce)
+namespace Dirscord_Bot.Class
+{
+    //Class
+    internal class CL_Websocket
+    {
+        //Class Variables
+        //Input
+
+        //Output
+
+        //Properties
+
+        //Internal
+
+        //-------------------------------------------------------------------
+        //Methodes
+
+
+
+        //Constructor 
+        CL_Websocket(
+        //Input
+        )
+        {
+            //Codespace             
+
+
+
+        }
+
+    }
+}
