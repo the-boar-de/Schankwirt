646144f6148b175ed43d539d11c6801fa3baf43d
The_Boar_DE <116942008+the-boar-de@users.noreply.github.com>
2025-12-04 15:31:50 +0100

    added Database connection

---
+++ Discord_Bot/Bot_Functions/SlashCommands/Database/User.cs
@@ -0,0 +1,21 @@
+//standard system refernces
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+//own refernces
+namespace DiscordBot.Database
+{
+    public class User
+    {
+        public int Id { get; set;}                 //Primaly key   
+        public ulong ChannelID {get; set;}         //ID of Channel
+        public ulong DiscordUserId {get; set;}     // ID of User
+        public string ?DiscordUserName {get; set;}  //Name of User
+
+    }   
+
+}
