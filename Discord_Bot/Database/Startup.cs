93f77696f3c2363a445d16cae57434a7b17f76ca
The_Boar_DE <116942008+the-boar-de@users.noreply.github.com>
2025-12-05 15:26:05 +0100

    Fix variable name typos in Startup.cs

--- Discord_Bot/Database/Startup.cs
+++ Discord_Bot/Database/Startup.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using DiscordBot.Database;
 
-namespace DiscordBot
+namespace DiscordBot.Database
 {
     public class Startup
     {
@@ -17,7 +17,7 @@ namespace DiscordBot
             //Database Port 
             var mariadb_port = Environment.GetEnvironmentVariable("MARIADB_PORT") ?? "3306";
             //Database user 
-            var mariaddb_user = Environment.GetEnvironmentVariable("MARIADB_USER") ?? "user";
+            var mariadb_user = Environment.GetEnvironmentVariable("MARIADB_USER") ?? "user";
             //Database password 
             var mariadb_password = Environment.GetEnvironmentVariable("MARIADB_PASSWORD") ?? "password";
 
@@ -25,10 +25,10 @@ namespace DiscordBot
             //Database - Logs
 
             //Data base Name 
-            var mariadb_databaselogs = Environment.GetEnvironmentVariable("MARIADB_DATABASELOGS") ?? "mariadb_databaselogs";
+            var mariadb_databaselogs = Environment.GetEnvironmentVariable("MARIADB_DATABASE") ?? "logs";
 
             // Connection String
-            var connectionString = $"Server={mariadb_host};Port={mariadb_port};Database={mariadb_databaselogs};User={mariaddb_user};Password={mariadb_password};";
+            var connectionString = $"Server={mariadb_host};Port={mariadb_port};Database={mariadb_databaselogs};User={mariadb_user};Password={mariadb_password};";
 
             // Server Version
             var serverVersion = ServerVersion.AutoDetect(connectionString);
