5419094b816f8469cc673c420e6aa5968975bee8
Ck7296 <chris.kett07@gmail.com>
2026-01-09 18:56:37 +0100

    aenderung

---
+++ Schankwirt/Migrations/20260109110743_AdditionalInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Schankwirt.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdditionalInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AdditionalInfo",
+                table: "Logs",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Logs",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdditionalInfo",
+                table: "Logs");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Logs");
+        }
+    }
+}
