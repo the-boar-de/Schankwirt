b7b4a53bb13630c5e38c5b6a2270d50f410f4ce5
The_Boar_DE <116942008+the-boar-de@users.noreply.github.com>
2026-06-01 21:45:14 +0200

    added initial sequence
    fix #24

---
+++ Functions/InitialSequence.cs
@@ -0,0 +1,16 @@
+//standard system refernces
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+//own refernces
+
+//project refernces
+using Discord;
+using Discord.Interactions;
+
+class InitialSequence
+{
+    
+}
