2ac9592d58ce1f786d40b34398e0add9c66c2f76
The_Boar_DE <116942008+the-boar-de@users.noreply.github.com>
2026-06-01 21:06:56 +0200

    added new class to create rolles

---
+++ Functions/CreateNewRole.cs
@@ -0,0 +1,20 @@
+//standard system refernces
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+//own refernces
+
+//project refernces
+using Discord;
+using Discord.Interactions;
+
+
+namespace Boxes
+{
+    public class CreateNewRole
+    {
+        
+    }
+}
