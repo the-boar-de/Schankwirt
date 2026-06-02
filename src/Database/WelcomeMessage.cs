f05d37b28cbe3d6cca496c028a742d70e9c29dec
The_Boar_DE <116942008+the-boar-de@users.noreply.github.com>
2026-06-03 00:06:55 +0200

    added WelcomeMessage tabel

---
+++ src/Database/WelcomeMessage.cs
@@ -0,0 +1,24 @@
+//standard system refernces
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+//own refernces
+namespace Schankwirt.Database
+{
+    public class WelcomeMessage
+    {
+        public int Id { get; set;}                 //Primaly key      
+        public string WelcomeMessage {get; set;}      //Welcomemessage
+        public string ?DiscordUserName {get; set;}  //Name of User
+        public string ?CommandId {get; set;}      //Command that was used
+        public string ?AdditionalInfo {get; set;} //Additional Info
+        public DateTime CreatedAt {get; set;} //Timestamp
+
+    }   
+
+}
